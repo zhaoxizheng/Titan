e45e6fdc9024787a16cb4efbf9cd82534b10cded
zhaoxizheng <zhaoxi@umich.edu>
2018-03-03 16:28:53 -0500

    Update RecommendItem.java

--- src/rpc/RecommendItem.java
+++ src/rpc/RecommendItem.java
@@ -21,13 +21,13 @@ import entity.Item;
 public class RecommendItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RecommendItem() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+        /**
+         * @see HttpServlet#HttpServlet()
+         */
+        public RecommendItem() {
+            super();
+            // TODO Auto-generated constructor stub
+        }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
