449ea3ca9c4d57ba1f4729417b9c00bac26c4c74
zhaoxizheng <zhaoxi@umich.edu>
2018-03-03 17:25:12 -0500

    Update DBConnection.java

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -71,8 +71,7 @@ public interface DBConnection {
 	public void saveItem(Item item);
 
 	/**
-	 * Get full name of a user. (This is not needed for main course, just for demo
-	 * and extension).
+	 * Get full name of a user. 
 	 * 
 	 * @param userId
 	 * @return full name of the user
@@ -80,8 +79,7 @@ public interface DBConnection {
 	public String getFullname(String userId);
 
 	/**
-	 * Return whether the credential is correct. (This is not needed for main
-	 * course, just for demo and extension)
+	 * Return whether the credential is correct. 
 	 * 
 	 * @param userId
 	 * @param password
